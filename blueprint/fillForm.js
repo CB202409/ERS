7396388a45895389a0f26fb56c6b57f4804c4c2f
SBShin92 <aopaoi0987@gmail.com>
2024-09-11 16:19:31 +0900

    CSS셀렉터 API 완성되면 동작하는 코드

---
+++ blueprint/fillForm.js
@@ -0,0 +1,65 @@
+// 현재 페이지에서 Tag 정보 가져와 생성형AI에게 정리를 맡겨 받는 함수
+async function autoFillFormHandler() {
+  // console.log(pageTagInfo);
+  // keys = ["userInfo"];
+  
+  /* 현재 페이지에서 form 다 가져와 formSum에 텍스트로 저장 */
+  const forms = document.querySelectorAll("form");
+  let formSum = "";
+  forms.forEach((form) => {
+    formSum += form.innerHTML;
+  });
+  
+  /* formSum을 서버로 전송(Gemini) */
+  parserURL = "http://127.0.0.1:8001/";
+  pageTagInfo = await fetchPostToJson(parserURL, formSum);
+  console.log(pageTagInfo);
+
+  console.log("밖1");
+  /* 크롬 스토리지에서 데이터 가져오기 */
+  keys = ["userInfo"];
+  const userInfo = await new Promise((resolve, reject) => {
+    chrome.storage.sync.get(keys, (result) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(result.userInfo);
+      }
+    });
+  });
+  console.log(userInfo);
+  console.log("밖2");
+
+
+  /* userInfo값과 pageTagInfo값을 이용, 현재 페이지 채우기 */
+  for (const [key, cssSelector] of Object.entries(pageTagInfo.personalInfo)) {
+    if (cssSelector) {
+      const inputElement = document.querySelector(cssSelector);
+      if (inputElement) {
+        inputElement.value = userInfo.personalInfo[key] || '';
+      }
+    }
+  }  
+}
+
+async function fetchPostToJson(fetchURL, inputString) {
+  try {
+    const sendData = { param1: inputString };
+
+    const response = await fetch(fetchURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(sendData)
+    });
+    const result = await response.json();
+    alert("완료!");
+    return result;
+  } catch (error) {
+    alert(error);
+    console.log(error);
+  }
+}
+
+autoFillFormHandler();
--- blueprint/popup.js
+++ blueprint/popup.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       fetchURL = event.target.fetchURL?.value;
       imageData = event.target.uploadImage?.files[0];
       if (fetchURL && imageData) {
-        data = await fetchURLWithImageHandler(fetchURL, imageData);
+        data = await fetchPostToJson(fetchURL, imageData);
         if (data) {
           console.log(data);
           setChromeStorage(data);
@@ -26,27 +26,17 @@ document.addEventListener("DOMContentLoaded", () => {
       " btn-success disabled";
   });
 
-  // 크롬 스토리지에 있는 내용을 dummy-html에 내보내기
+  // 크롬 스토리지에 있는 내용을 현재 페이지에 내보내기
   document.getElementById("exportbtn1").addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
-        function: fillFormHandler,
+        files: ["fillForm.js"],
       });
     });
   });
 });
 
-// TODO: 태그 역시 JSON에서 가져와야함
-function fillFormHandler() {
-  chrome.storage.sync.get(["userInfo"], (result) => {
-    document.getElementById("name").value = result.userInfo.personalInfo.lastName + result.userInfo.personalInfo.firstName;
-    document.getElementById("dateOfBirth").value = result.userInfo.personalInfo.dateOfBirth;
-    document.getElementById("email").value = result.userInfo.personalInfo.email;
-    document.getElementById("address").value = result.userInfo.personalInfo.address;
-    document.getElementById("phoneNumber").value = result.userInfo.personalInfo.phoneNumber;
-  });
-}
 
 // 크롬 스토리지 세팅하는 함수
 function setChromeStorage(obj) {
@@ -56,29 +46,13 @@ function setChromeStorage(obj) {
   console.log("chrome storage saved!!!");
 }
 
-// post로 fetch만 하는 함수
-// async function fetchURLHandler(fetchURL) {
-//   try {
-//     const response = await fetch(fetchURL, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-//     const result = await response.json();
-//     alert("완료!");
-//     return result;
-//   } catch (error) {
-//     alert(error);
-//     console.log(error);
-//   }
-// }
-
 // 이미지와 함께 fetch
-async function fetchURLWithImageHandler(fetchURL, imageData) {
+async function fetchPostToJson(fetchURL, imageData) {
   try {
     const formData = new FormData();
-    formData.append("file", imageData);
+    if (imageData) {
+      formData.append("file", imageData);
+    }
 
     const response = await fetch(fetchURL, {
       method: "POST",
