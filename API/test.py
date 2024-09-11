08a88f02c4d41812fdc993d6a2f5e8580833219c
parkmj9515 <163090357+parkmj9515@users.noreply.github.com>
2024-09-11 14:12:05 +0900

    API 프롬프트 수정 태그별로 모음

--- API/test.py
+++ API/test.py
@@ -11,7 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Google Gemini API 키 설정
-genai.configure(api_key="")
+genai.configure(api_key="AIzaSyAsPTSBN5PUXbafzKMYLo-H6jwlCf5Am9k")
 
 # 파일을 Gemini에 업로드하는 함수
 def upload_to_gemini(file_path, mime_type=None):
@@ -49,30 +49,36 @@ async def process_resumes(file1: UploadFile = File(...)):
             model_name="gemini-1.5-flash",
             generation_config=generation_config_1,
             system_instruction="""
-            CSS Selector 형태로 출력해줬으면 좋겠어
-            
-            예를들면
-            {
-            "name":"이름"
-            "birth":"생년월일"
-            "phone":"전화번호"
-            ...
-            }
-            이런식으로 출력해줬으면 좋겠어
-
-            그리고 기존 데이터는 그대로 출력해야 하며, 새로운 정보를 입력하는 부분만 충전하세요.
-            없는 정보는 빈 문자열("") 로 표시해주세요.
-            필수정보를 교체하세요
-            기존에 추출한 정보는 새로운 정보로 대체하겠습니다. 
-
-            새로운 필수정보는 다음과 같습니다
-            {
-                "이름": "장보고",
-                "전화": "010-2222-1111",
-                "이메일": "woja@cas.com"
+            해당파일을 올리면 태그별로 모아줬으면 좋겠어
+            특히 기본정보인 userInfo{ firstname,lastname,email,phone_number,address,bitrhday,gender...} 는 userInfo객체에 넣을거고
+            pageInfo객체도 만들어서 내부에 {edu, persnalInfo, cetificatin ... } 정보를 출력해줬으면 좋겠어
+            특히, cetification에는 경력 정보를 넣어줬으면 좋겠어
+            그리고 firstname, lastname도 나누어 주어야하는데 만약에 한글이름이 홍길동이면 홍=firstname, 길동=lastname 이야
+            그런데 성,이름을 따로 적지않고 이름 만 있는경우는 그대로 name만 출력해 주면돼
+            또한 형식은 
+            userInfo{
+                "FirstName":"input[name='first-name']",
+                "LirstName":"input[name='last-name']",
+                "dateOfBirth":"input[name='birth date'],
+                "age":"input[name='age']",
+                "email":"input[name='email']",
+                "phoneNumber":"input[name='phone-number']",
+                "address":"input[name='address']",
                 ...
             }
-            위와 같은 정보를 입력해야 합니다.
+            pageTagInfo{
+               "education":"input[name='education']",
+               "personalInfo":"input[name='personal-info']",
+                "cetificatin":"input[name='certification']"
+                ...
+                }
+            이런 형식으로 출력되었으면 좋겠어 
+            빈칸인 경우는 "" 이렇게 출력되거나 비어있었으면 좋겠어 
+            또한 cetification의 경우 경력이 하나면 cetificatin:[] 이렇게 하나, 
+            두개이상이면 {
+                            cetification:[],
+                            cetification:[]
+                } 이런식으로 줄 단위로 나왔으면 좋겠어 
             
     
             """
