b43af693a810b158f62e7a1e096bb3a53958063e
SBShin92 <aopaoi0987@gmail.com>
2024-09-24 14:13:31 +0900

    STT 삭제

--- rag_fastapi/stt/stt.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, WebSocket
-import whisper  # openai-whisper
-
-
-router = APIRouter(tags=["Speach-To-Text"])
-
-@router.websocket("/transcribe")
-async def transcribe():
-    pass
