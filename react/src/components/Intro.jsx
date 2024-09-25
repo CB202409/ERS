59fa35ae8787faf91108be743bfae517c746bda7
SBShin92 <aopaoi0987@gmail.com>
2024-09-25 17:52:44 +0900

    클래스 class -> className으로 수정(리액트 호환)

--- react/src/components/Intro.jsx
+++ react/src/components/Intro.jsx
@@ -5,27 +5,27 @@ import about from "../assets/img/about.jpg";
 const Intro = () => {
     return (
         <section id="intro">
-            <div class="intro__inner">
-                <div class="intro__lines" aria-hidden="true">
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
+            <div className="intro__inner">
+                <div className="intro__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
                 </div>
-                    <div class="img">
+                    <div className="img">
                         <img src={about} alt="어바웃" />
                     </div>
                 </div>
-                <div class="intro__lines bottom" aria-hidden="true">
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
-                    <span class="line"></span>
+                <div className="intro__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
                 </div>
         </section>
     );
