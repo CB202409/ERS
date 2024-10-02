f18d27c7518145a50fcba27553243ca6a7db0034
SBShin92 <aopaoi0987@gmail.com>
2024-10-02 10:40:41 +0900

    delete stuff
    안쓰는파일 모두 삭제

--- mylang/app/pdf_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def list_files_with_paths(directory):
-    file_list = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            file_list.append(os.path.join(root, file))
-    return file_list
