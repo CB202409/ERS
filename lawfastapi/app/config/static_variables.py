2b4ac8bc2ac20707751e06ce89323cac0fdc8237
SBShin92 <aopaoi0987@gmail.com>
2024-10-17 17:56:43 +0900

    fastapi 최신화(.env, 파일정리, requirement.txt추가 등)

--- lawfastapi/app/config/static_variables.py
+++ lawfastapi/app/config/static_variables.py
@@ -1,17 +1,20 @@
 class StaticVariables:
     # AI모델
-    OPENAI_MODEL = "gpt-4o-mini"
-    REWRITE_MODEL = "gpt-4o-mini"
+    OPENAI_MODEL = "gpt-4o"
+    REWRITE_MODEL = "gpt-4o"
     
     # 임베딩 설정
     UPSTAGE_EMBEDDING_MODEL = "embedding-passage"
     UPSTAGE_RETRIEVE_MODEL = "embedding-query"
-    CHUNK_SIZE = 400
-    CHUNK_OVERLAP = 50
+    CHUNK_SIZE = 730
+    CHUNK_OVERLAP = 60
+    
+    # LangSmith 로그 이름
+    LANGSMITH_LOG_TITLE="Law-RAG-API"
     
     # Retrieval
-    RETRIEVAL_K = 10  # Top-K 문서 반환 개수
-    RETRIEVAL_ALPHA = 0.5  # alpha=0.75로 설정한 경우, (0.75: Dense Embedding, 0.25: Sparse Embedding)
+    RETRIEVAL_K = 11  # Top-K 문서 반환 개수
+    RETRIEVAL_ALPHA = 0.42  # alpha=0.75로 설정한 경우, (0.75: Dense Embedding, 0.25: Sparse Embedding)
     
     # pinecone
     PINECONE_INDEX_NAME = "law-pdf"
@@ -23,10 +26,9 @@ class StaticVariables:
     SQLITE_DB_PATH = "./db/chat_history.db"
     
     # OpenAI 어시스턴트용
+    OPENAI_ASSISTANT_CHECKER_MODEL = "gpt-4o-mini"
     OPENAI_ASSISTANT_MODEL = "gpt-4o"
     OPENAI_ASSISTANT_NAME = "계산"
-    OPENAI_ASSISTANT_ID = "asst_2DyKo0eGyav2JL7JAH31TrbH"
     OPENAI_ASSISTANT_TEMPERATURE = 0.01
     OPENAI_ASSISTANT_TOP_P = 0.95
     
-    OPENAI_THREAD_ID = "thread_3SIL0gL4BIhm9rRx6Q3L26N1"
